f3c485d5bbd039b27bc8e2a1707769637d3cc073
Frank <zhiguo.zhang@mail.mcgill.ca>
2018-01-29 19:55:57 -0500

    Added Server comment

--- src/main/java/socs/network/node/Server.java
+++ src/main/java/socs/network/node/Server.java
@@ -4,12 +4,20 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-
+/**
+ * Implementation of a runnable server class for socket programming
+ * @author Zhiguo Zhang; Zhaoqi Xu
+ */
 public class Server implements Runnable{
     private ServerSocket serverSocket;
     private Router router;
     private boolean on = true;
 
+    /**
+     * Constructor for Server class
+     * @param port the process port number of server
+     * @param router the router instance which has the server running
+     */
     public Server(int port, Router router){
         this.router = router;
         try {
@@ -19,29 +27,47 @@ public class Server implements Runnable{
         }
     }
 
+    /**
+     * Method to turn off server
+     */
     public void turn_off() {
         this.on = false;
     }
+
+    /**
+     * Method implementing Runnable interface
+     * Accepting income client connect() calls
+     */
     public void run() {
         while (on) {
             try {
                 Socket clientSocket = serverSocket.accept();
-                ClientThread clientHandle = new ClientThread(clientSocket);
-                clientHandle.start();
+                ClientServiceThread client = new ClientServiceThread(clientSocket);
+                client.start();
             } catch (Exception e) {
                 if (e instanceof IOException)
                     System.out.println("Fail to accept");
             }
         }
     }
-    class ClientThread extends Thread {
+
+    class ClientServiceThread extends Thread {
         private Socket clientSocket;
         private ObjectInputStream in;
         private ObjectOutputStream out;
-        public ClientThread(Socket s) {
+
+        /**
+         * Constructor for ClientServiceThread
+         * @param s the client socket to listen
+         */
+        public ClientServiceThread(Socket s) {
+            super();
             clientSocket = s;
         }
 
+        /**
+         * Method invoked by Thread.start()
+         */
         @Override
         public void run() {
             try {
